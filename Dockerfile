8714ba92751f6c5a864027d830fa94b3c145bf5d
Adrian <adrian.rom64@gmail.com>
2021-06-28 15:30:23 +0200

    add docker

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM node:15.1.0-alpine
+
+WORKDIR /app
+
+COPY package.json .
+
+RUN apk add git
+
+RUN npm install --verbose
+
+ADD . .
+
+RUN npm run build
+
+CMD [ "node", "dist/src/main" ]
--- src/main.ts
+++ src/main.ts
@@ -3,25 +3,30 @@ configure()
 
 import * as Blockchain from './Blockchain'
 import * as Database from './Database'
-import { UpdateDeviceHandler } from './TxHandlers/DeviceHandler'
-import { SubscribeEvent } from './Types'
-import { DappFatherHandler } from './TxHandlers/DappFatherHandler'
-import { UpdateSupplierHandler } from './TxHandlers/SupplierHandler'
-import { handleKeyTransferUpdates, handleKeyUpdates } from './TxHandlers/KeyHandler'
+import { parseUpdate } from './UpdateParser'
+import type { SubscribeEvent } from './Types'
+
+import { handleDappFatherUpdates } from './TxHandlers/DappFatherHandler'
+import { handleDeviceUpdates } from './TxHandlers/DeviceHandler'
+import { handleEventUpdates } from './TxHandlers/EventHandler'
+import { handleKeyUpdates } from './TxHandlers/KeyHandler'
+import { handleOrganisationUpdates } from './TxHandlers/OrganisationHandler'
+import { handleSupplierUpdates } from './TxHandlers/SupplierHandler'
 
 const handle = async (chunk: SubscribeEvent) => {
-  await DappFatherHandler.handle(chunk)
-  await UpdateSupplierHandler.handle(chunk)
-  await UpdateDeviceHandler.handle(chunk)
-  await handleKeyUpdates(chunk)
-  await handleKeyTransferUpdates(chunk)
+  const update = parseUpdate(chunk)
+
+  await handleDappFatherUpdates(update)
+  await handleDeviceUpdates(update)
+  await handleEventUpdates(update)
+  await handleKeyUpdates(update)
+  await handleOrganisationUpdates(update)
+  await handleSupplierUpdates(update)
 }
 
 const _ = (async () => {
   await Database.connect()
-
   const height = await Blockchain.fetchHeight()
-  // const res = await Blockchain.fetchAsset('CGqeZZftS13JtEztuZFr7MwV2QLqLsNCwQpnQm3vNXT2')
 
   Blockchain.subscribe(handle, height)
 })()
